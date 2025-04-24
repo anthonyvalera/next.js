0d20f7d6f4f689931cbf9e6483e0dd939044e56b
devjiwonchoi <devjiwonchoi@gmail.com>
2025-04-24 22:14:38 +0900

    [ts-next-plugin] add test

---
+++ test/development/typescript-plugin/index.test.ts
@@ -0,0 +1,104 @@
+import tsNextPluginFactory from 'next'
+import ts from 'typescript'
+import {
+  createSystem,
+  createVirtualLanguageServiceHost,
+  createDefaultMapFromNodeModules,
+  createVirtualTypeScriptEnvironment,
+} from '@typescript/vfs'
+import { resolve } from 'path'
+
+// const configFile = ts.readConfigFile(
+//   resolve(__dirname, 'tsconfig.json'),
+//   ts.sys.readFile
+// )
+// const { options: compilerOptions } = ts.parseJsonConfigFileContent(
+//   configFile.config,
+//   ts.sys,
+//   __dirname
+// )
+
+const rootDir = resolve(__dirname, '..', '..', '..')
+const { name } = require(resolve(rootDir, 'package.json'))
+if (name !== 'nextjs-project') {
+  throw new Error(
+    'This script must be run from the root of the Next.js project'
+  )
+}
+
+const compilerOptions = ts.getDefaultCompilerOptions()
+
+const files = ts.sys.readDirectory(resolve(__dirname, 'app'))
+const tsLibDirectory = resolve(rootDir, 'node_modules/typescript/lib')
+const fsMap = createDefaultMapFromNodeModules(
+  compilerOptions,
+  ts,
+  tsLibDirectory
+)
+
+files.forEach((filePath) => {
+  const contents = ts.sys.readFile(filePath)
+  if (contents) {
+    fsMap.set(filePath, contents)
+  }
+})
+
+const system = createSystem(fsMap)
+
+const env = createVirtualTypeScriptEnvironment(
+  system,
+  files,
+  ts,
+  compilerOptions
+)
+const { languageServiceHost } = createVirtualLanguageServiceHost(
+  ts.sys,
+  files,
+  compilerOptions,
+  ts
+)
+
+// const compilerHost = ts.createCompilerHost(compilerOptions)
+// const languageServiceHost: ts.LanguageServiceHost = {
+//   ...compilerHost,
+//   getCompilationSettings: () => compilerOptions,
+//   getScriptFileNames: () => files,
+//   getScriptSnapshot: (fileName) => {
+//     const contents = ts.sys.readFile(fileName)
+//     if (contents && typeof contents === 'string') {
+//       return ts.ScriptSnapshot.fromString(contents)
+//     }
+//     return
+//   },
+//   getScriptVersion: () => '0',
+//   writeFile: ts.sys.writeFile,
+// }
+// const languageService = ts.createLanguageService(languageServiceHost)
+const plugin: ts.server.PluginModule = (
+  tsNextPluginFactory as unknown as ts.server.PluginModuleFactory
+)({ typescript: ts })
+
+beforeEach(() => {
+  jest.spyOn(console, 'log').mockImplementation()
+})
+
+const pluginCreateInfo: ts.server.PluginCreateInfo = {
+  project: {
+    projectService: {
+      logger: {
+        info: console.log,
+      },
+    },
+    getCurrentDirectory: () => __dirname,
+  } as unknown as ts.server.Project,
+  languageService: env.languageService,
+  languageServiceHost,
+  serverHost: null,
+  config: {},
+}
+const pluginLanguageService = plugin.create(pluginCreateInfo)
+const pagePath = resolve(__dirname, 'app/metadata/client/page.tsx')
+console.log({ pagePath })
+const semanticDiagnostics =
+  pluginLanguageService.getSemanticDiagnostics(pagePath)
+console.log(semanticDiagnostics)
