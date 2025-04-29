bbab6fb0be6fb57e19c0da007733d21ae1ede123
JJ Kasper <jj@jjsweb.site>
2025-04-29 09:43:48 -0700

    Change stylistic ESLint TypeScript defaults

--- packages/eslint-config-next/typescript.js
+++ packages/eslint-config-next/typescript.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warning',
+    '@typescript-eslint/no-unused-expressions': 'warning',
+  },
 }
